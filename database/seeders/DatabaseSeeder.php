7bab5151da9430ecfbb54bbc78824949b86f8336
wholespace214 <wholespace.214@gmail.com>
2022-01-14 15:25:31 +0300

    Get rid off demo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            AdminSeeder::class,
-            DemoSeeder::Class
+            AdminSeeder::class
         ]);
     }
 }
