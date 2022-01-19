c4b9961132c3504cec5684ec574a7805962090f7
wholespace214 <wholespace.214@gmail.com>
2022-01-19 15:56:38 +0300

    CSRF are causing issues with tokens

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             \App\Http\Middleware\ForceJson::class,
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
