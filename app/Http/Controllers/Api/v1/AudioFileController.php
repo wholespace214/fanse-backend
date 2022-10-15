d42a782b44f9c431dc0c8ca6efb470809bb30785
wholespace214 <wholespace.214@gmail.com>
2022-10-14 20:50:02 -0400

    Update to Current

--- app/Http/Controllers/Api/v1/AudioFileController.php
+++ app/Http/Controllers/Api/v1/AudioFileController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AudioFileController extends Controller
 {
     public function saveFile(Request $request)
     {
-        $upload_path = public_path('audio');
-        $file_name = $request->file->getClientOriginalName();
         $generated_new_name = time() . '.' . $request->file->getClientOriginalExtension();
-        $request->file->move($upload_path, $generated_new_name);
+        $request->file->storeAs('tmp', $generated_new_name);
+
+        $s3_filepath = 'bio_voice/' . $generated_new_name;
+        Storage::disk('s3')->put($s3_filepath, file_get_contents($request->file));
+
         return response()->json([
-            'audio' => "/audio/" . $generated_new_name   
+            'audio' => "/bio_voice/" . $generated_new_name
         ]);
     }
 }
